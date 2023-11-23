ab26ba11aea902c1cf881fe2218d40df54550a30
funzter <cavehive@protonmail.com>
2023-11-23 23:50:19 +0100

    fixed user settings

--- src/main/java/de/timesnake/web/views/user/UserMenu.java
+++ src/main/java/de/timesnake/web/views/user/UserMenu.java
@@ -101,7 +101,6 @@ public class UserMenu extends Div {
     TextField name = new TextField("Name");
     PasswordField password = new PasswordField("Password");
 
-
     formLayout.add(username, name, password);
     editorDiv.add(formLayout);
 
@@ -110,6 +109,8 @@ public class UserMenu extends Div {
     binder.bind(name, "name");
     binder.bind(password, u -> "********",
         (u, p) -> u.setHashedPassword(this.securityConfiguration.passwordEncoder().encode(p)));
+
+    binder.readBean(this.user);
 
     HorizontalLayout buttonLayout = new HorizontalLayout();
     buttonLayout.setClassName("button-layout");
