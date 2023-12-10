3fb8098a37a7d0ae764934d090098e5c9ac49c06
funzter <cavehive@protonmail.com>
2023-12-10 22:05:17 +0100

    added database files

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO print_server.application_user (id, hashed_password, name, profile_picture, username)
+VALUES (1, '$2a$10$1TkSkZuNSr1TsIMVQeE56ufjXA9lSfS8GyZUzE4IAm8lB3XvqD40.', 'a', NULL, 'a');
